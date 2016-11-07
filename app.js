1367c7f4b687e077744aaf8a63ab70fa6b1900f1
Lei Xu <Layric@Leis-MacBook-Pro.local>
2016-11-07 00:36:27 -0800

    lei

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ var mongoose = require("mongoose");
 //    else console.log("Connected to MongoDb")
 //});
 
-mongoose.connect('mongodb://heroku_vmvnkp2l:um0rgahqlheiog2j8k7qmb2h1p@ds027908.mongolab.com:27908/heroku_vmvnkp2l',function(err){
+mongoose.connect('mongoDB address',function(err){
   if(err){
-    console.log('Could not connect to mongodb on localhost');
+    console.log('Could not connect to mongodb');
   }else{
     console.log('Successfully connected to mongodb');
   }
